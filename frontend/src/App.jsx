e46aa9e6967a1d2297c7a19bf42663e232f597b6
AJAY <ajay.bhalekar24@vit.edu>
2024-12-21 19:25:17 +0530

    asdj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./TokenPurchase.css";
+import "../TokenPurchase.css";
+
 
 const TokenPurchase = () => {
   const [tokenAmount, setTokenAmount] = useState(0);
